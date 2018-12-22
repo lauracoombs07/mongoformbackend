e4e1ffb409f45b32888158251cc39e4279d0ef4b
Laura <laura.coombs07@gmail.com>
2018-12-22 08:26:17 -0700

    express activated

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-// const express = require('express')
-// const app = express()
+const express = require('express')
+const app = express()
 const bodyParser = require('body-parser')
 const monk = require('monk')
 const url = 'mongodb://admin:admin123@ds157549.mlab.com:57549/bradtutorial';//admin admin123 is the user and pass of the collection
@@ -14,7 +14,7 @@ db.then(() => {
   
 const spiderman = db.get('people')//variable can be named anything but needs to call('')exactly what is in the DB
 const batman = db.get('email')
-const users = db.get('users', options)
+const users = db.get('users')
 app.use(bodyParser.json())//takes strings and turns them into JSON
 app.use(cors())//so I can use 2 local hosts at the same time
 
@@ -23,15 +23,13 @@ app.use(cors())//so I can use 2 local hosts at the same time
 
 app.get('/', function (req, res) {
     res.send('GET request to the homepage')
-  })
+  });
   
 //   // POST method route
   app.post('/', function (req, res) {
     const result = spiderman.insert(req.body)
     res.status(200).send(result)
-  })
-
-
+  });
 
     
 
